7498e2fab544749bae34e4f1fcf9159b3e576d5d
guxukai <gxk@33.cn>
2022-01-27 16:52:38 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+const AutoImport = require('unplugin-auto-import/webpack')
 module.exports = {
   pluginOptions: {
     electronBuilder: {
@@ -18,4 +21,31 @@ module.exports = {
   },
 
   transpileDependencies: ["quasar"],
+  configureWebpack: {
+    plugins: [
+      Components({
+        resolvers: [ElementPlusResolver()],
+        dirs: [
+          'src/components',
+        ],
+        dts: true,
+        directoryAsNamespace: true,
+      }),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+        include: [
+          /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+          /\.vue$/,
+          /\.vue\?vue/, // .vue
+          /\.md$/, // .md
+        ],
+        imports: ['vue', 'vue-router', 'vue-i18n', '@vueuse/core', 'pinia'],
+        eslintrc: {
+          enabled: true, // Default `false`
+          filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+          globalsPropValue: 'readonly', // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+        },
+      }),
+    ]
+  }
 };
